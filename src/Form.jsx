6c1b30f1365e63f92a4c874ca1a00264fa19db9c
kirylkuchko <kirylkuchko@gmail.com>
2025-09-30 14:05:58 +0200

    Refactor Formik hook on component

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,99 +1,91 @@
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const Form = () => {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            amount: 0,
-            currency: '',
-            text: '',
-            terms: false
-        },
-        validationSchema: Yup.object({
-            name: Yup.string()
-                .min(2, 'Minimum 2 symbols')
-                .required('Required'),
-            email: Yup.string()
-                .email('Invalid email address')
-                .required('Required'),
-            amount: Yup.number()
-                .min(5, 'Minimum 5')
-                .required('Required'),
-            currency: Yup.string()
-                .required('Select currency'),
-            text: Yup.string()
-                .min(10, 'Minimum 10 symbols'),
-            terms: Yup.boolean()
-                .required('Acceptance required').
-                oneOf([true], 'Acceptance required')
-        }),
-        onSubmit: values => console.log(JSON.stringify(values, null, 2))
-    });
-
-    const getErrorMessageElement = (fieldName) => {
-        return formik.touched[fieldName] && formik.errors[fieldName] 
-            ? <div className='error'>{formik.errors[fieldName]}</div> 
-            : null;
-    }
-
+const CustomForm = () => {
     return (
-        <form className="form" onSubmit={formik.handleSubmit}>
-            <h2>Send a donation</h2>
-            <label htmlFor="name">Your name</label>
-            <input
-                id="name"
-                name="name"
-                type="text"
-                {...formik.getFieldProps('name')}
-            />
-            { getErrorMessageElement('name') }
-            <label htmlFor="email">Your email</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                {...formik.getFieldProps('email')}
-            />
-            { getErrorMessageElement('email') }
-            <label htmlFor="amount">Amount</label>
-            <input
-                id="amount"
-                name="amount"
-                type="number"
-                {...formik.getFieldProps('amount')}
-            />
-            { getErrorMessageElement('amount') }
-            <label htmlFor="currency">Currency</label>
-            <select
-                id="currency"
-                name="currency"
-                {...formik.getFieldProps('currency')}>
-                    <option value="">Select currency</option>
-                    <option value="USD">USD</option>
-                    <option value="EUR">EUR</option>
-                    <option value="PLN">PLN</option>
-            </select>
-            { getErrorMessageElement('currency') }
-            <label htmlFor="text">Your message</label>
-            <textarea 
-                id="text"
-                name="text"
-                {...formik.getFieldProps('text')}
-            />
-            { getErrorMessageElement('text') }
-            <label className="checkbox">
-                <input 
-                    name="terms" 
-                    type="checkbox" 
-                    {...formik.getFieldProps('terms')}/>
-                You agree with privacy policy?
-            </label>
-            { getErrorMessageElement('terms') }
-            <button type="submit">Send</button>
-        </form>
+        <Formik
+            initialValues = {{
+                name: '',
+                email: '',
+                amount: 0,
+                currency: '',
+                text: '',
+                terms: false
+            }}
+            validationSchema = {
+                Yup.object({
+                    name: Yup.string()
+                        .min(2, 'Minimum 2 symbols')
+                        .required('Required'),
+                    email: Yup.string()
+                        .email('Invalid email address')
+                        .required('Required'),
+                    amount: Yup.number()
+                        .min(5, 'Minimum 5')
+                        .required('Required'),
+                    currency: Yup.string()
+                        .required('Select currency'),
+                    text: Yup.string()
+                        .min(10, 'Minimum 10 symbols'),
+                    terms: Yup.boolean()
+                        .required('Acceptance required').
+                        oneOf([true], 'Acceptance required')
+            })}
+            onSubmit = {values => console.log(JSON.stringify(values, null, 2))}
+        >
+            <Form className="form">
+                <h2>Send a donation</h2>
+                <label htmlFor="name">Your name</label>
+                <Field
+                    id="name"
+                    name="name"
+                    type="text"
+                />
+                <ErrorMessage className="error" name="name" component="div"/>
+                <label htmlFor="email">Your email</label>
+                <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                />
+                <ErrorMessage className="error" name="email" component="div"/>
+                <label htmlFor="amount">Amount</label>
+                <Field
+                    id="amount"
+                    name="amount"
+                    type="number"
+                />
+                <ErrorMessage className="error" name="amount" component="div"/>
+                <label htmlFor="currency">Currency</label>
+                <Field
+                    id="currency"
+                    name="currency"
+                    as="select">
+                        <option value="">Select currency</option>
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="PLN">PLN</option>
+                </Field>
+                <ErrorMessage className="error" name="currency" component="div"/>
+                <label htmlFor="text">Your message</label>
+                <Field 
+                    id="text"
+                    name="text"
+                    as="textarea"
+                />
+                <ErrorMessage className="error" name="text" component="div"/>
+                <label className="checkbox">
+                    <Field 
+                        name="terms" 
+                        type="checkbox" 
+                        />
+                    You agree with privacy policy?
+                </label>
+                <ErrorMessage className="error" name="terms" component="div"/>
+                <button type="submit">Send</button>
+            </Form>
+        </Formik>
     )
 }
 
-export default Form;
+export default CustomForm;
